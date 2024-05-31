0308fec8a2835a42757ebcb21283e09b6dab8e1e
shimae naga <isagi.kazui@gmail.com>
2024-05-31 11:27:39 +0900

    20240531

--- reserveDetail.php
+++ reserveDetail.php
@@ -1,3 +1,19 @@
+<?php
+  session_start();
+  require_once('./dbConfig.php');
+  $link = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+  if ($link == null) {
+    die("接続に失敗しました:" . mysqli_connect_error());
+  }
+  mysqli_set_charset($link, "utf8");
+  $room_no = $_GET['rno'];
+  $sql = "SELECT room_name FROM room WHERE room_no = {$room_no}";
+  $result = mysqli_query($link, $sql);
+  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+  $roomName = $row['room_name'];
+  $_SESSION['reserve']['roomno'] = $roomNo;
+  $reserveDay = $_SESSION['reserve']['day'];
+?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -37,8 +53,11 @@
             （※ がついている項目は入力必須項目です）</p>
             <h3>予約情報</h3>
             <table class="input">
-              <tr><th>お部屋名称</th><td>（　選択された部屋名称　）</td></tr>
-              <tr><th>宿泊日</th><td>（　入力された宿泊日　）</td></tr>
+              <tr><th>お部屋名称</th>
+                <td><?= $roomName ?></td></tr>
+              <tr><th>宿泊日</th>
+                <td><?php echo date('Y/m/d', strtotime($reserveDay)) ; ?></td>
+              </tr>
             </table><br>
             <h3>代表者情報</h3>
             <form method="post" action="reserveCheck.php">
@@ -55,7 +74,31 @@
                 <th>メールアドレス</th>
                 <td><input type="text" name="dmail" value=""></td>
               </tr>
-              </table>
+              </table><br>
+            <h3>予約詳細情報</h3>
+            <table class="input">
+              <tr>
+                <th>宿泊人数（※）</th>
+                <td><input type="text" name="reserveNumber" value=""></td>
+              </tr>
+              <tr>
+                <th>チェックイン予定時間（※）</th>
+                <td><select name="chekin">
+                  <option value="">時間を選択</option>
+                  <option value="15:00">15:00</option>
+                  <option value="16:00">16:00</option>
+                  <option value="17:00">17:00</option>
+                  <option value="18:00">18:00</option>
+                  <option value="19:00">19:00</option>
+                </select></td>
+              </tr>
+              <tr>
+                <th>連絡事項</th><td><textarea name="message" cols="40" rows="4"></textarea></td>
+              </tr>
+            </table><br>
+            <p>まだ予約は確定しておりません。次の画面で確定させてください。</p>
+            <input class="submit_a" type="submit" value="予約確認">
+            <input class="submit_b" type="button" value="前の画面に戻る" onclick="history.back();">
             </form>
           </section>    
         </article>
@@ -90,5 +133,9 @@
       <p>Copyright c 2016 Jikkyo Pension All Rights Reserved.</p>
   </footer>
     <!-- フッター：終了 -->
+<?php
+  mysqli_free_result($result);
+  mysqli_close($link);
+?>
   </body>
 </html>
